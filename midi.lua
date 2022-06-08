19d5176bae7c9b14810c50b6d28f953a6380433f
Chris Roerig <chris-roerig@users.noreply.github.com>
2022-06-08 07:10:46 -0500

    Fixed bug where tool would crash if saved preference of MIDI device is no longer available.
    removed accidental change

--- midi.lua
+++ midi.lua
@@ -5,13 +5,21 @@ NOTE_OFF = 0x80
 ALL_NOTE_OFF_1 = 0xB0
 ALL_NOTE_OFF_2 = 0x7B
 
+
 -- get midi device name
 function select_midi_device(dev_index)
+  print("dev index:"..dev_index)
   STATE.midi_device = renoise.Midi.available_output_devices()[dev_index]
   STATE.midi_device_index = dev_index
   prefs:write("midi_device_index", dev_index)
   
   print("Selected device: "..STATE.midi_device)
+end
+
+-- get midi device object
+function get_midi_dev()
+  print("Getting device: "..STATE.midi_device)
+  return renoise.Midi.create_output_device(STATE.midi_device)
 end
 
 -- changes NOTE_ON and NOTE_OFF globals
